a143e76b969529ee74533e80d5570384aeb75159
Kate Bo <bodrakea@gmail.com>
2024-01-04 13:44:57 +0300

    initial commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,30 @@
+package org.example;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        while (true){
+            try {
+                int accountAmount;
+                System.out.println("Enter account balance:");
+                Scanner enteredValue = new Scanner(System.in);
+                int balance = enteredValue.nextInt();
+                System.out.println("Enter the amount to top up:");
+                int refill = enteredValue.nextInt();
+                if (refill>1000){
+                    accountAmount = balance + refill + refill/100;
+                }else {
+                    accountAmount = balance + refill;
+                }
+
+                System.out.println("In your account: " + accountAmount);
+                break;
+            }catch (InputMismatchException e) {
+                System.out.println("Please, enter the number");
+            }
+        }
+    }
+
+}
